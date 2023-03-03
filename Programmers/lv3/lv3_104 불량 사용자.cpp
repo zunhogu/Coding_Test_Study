f47e127755f33e63f53e170a93b30373c489f899
zunhogu <zunhogu@gmail.com>
2023-03-03 14:37:25 +0900

    20230303

--- Programmers/lv3/lv3_104 불량 사용자.cpp
+++ Programmers/lv3/lv3_104 불량 사용자.cpp
@@ -5,57 +5,36 @@
 #include <algorithm>
 using namespace std;
 
-void makeMap(vector<string>& user_id, vector<string>& banned_id, vector<vector<string>>& m)
+set<int> s;
+
+bool check(string str1, string str2)
 {
-    for(int i=0; i<banned_id.size(); i++)
+    if(str1.length() != str2.length()) return false;
+    
+    for(int i=0; i<str1.length(); i++)
     {
-        vector<string> temp;
-        for(int j=0; j<user_id.size(); j++)
-        {
-            bool equal = true;
-            if(banned_id[i].length() == user_id[j].length())
-            {
-                for(int n=0; n<banned_id[i].length(); n++)
-                {
-                    if(banned_id[i][n] == '*' || banned_id[i][n] == user_id[j][n]) continue;
-                    else equal = false;
-                }
-            }
-            else continue;
-            if(equal) temp.push_back(user_id[j]);
-        }
-        m.push_back(temp);
+        if(str2[i] == '*') continue;
+        else if(str1[i] != str2[i]) return false;
     }
+    return true;
 }
 
-void dfs(int index, vector<string>& user_id, vector<vector<string>>& m, int result)
+void dfs(vector<string>& user_id, vector<string>& banned_id, int index, int result)
 {
-    if(index >= m.size()-1)
+    if(index >= banned_id.size())
     {
-        cout <<result << endl;
+        s.insert(result);
         return;
     }
-    for(int i=index; i<m.size()-1; i++)
+    
+    for(int i=0; i<user_id.size(); i++)
     {
-        for(int j=0; j<m[i].size(); j++)
-        {
-            cout << m[i][j] << " ";
-            int idx = find(user_id.begin(), user_id.end(), m[i][j]) - user_id.begin();
-            
-                
-            result |= (1 << idx);
-            dfs(index+1, user_id, m, result);   
-        }
+        if(!(result & (1 << i)) && check(user_id[i], banned_id[index]))
+            dfs(user_id, banned_id, index+1, result | (1 << i));
     }
 }
 
 int solution(vector<string> user_id, vector<string> banned_id) {
-    int answer = 0;
-    vector<vector<string>> m;
-    set<int> counts;
-    makeMap(user_id, banned_id, m);
-
-    dfs(0, user_id, m, 0);
-    
-    return answer;
+    dfs(user_id, banned_id, 0, 0);
+    return s.size();
 }
