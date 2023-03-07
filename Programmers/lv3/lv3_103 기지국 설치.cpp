56e0df17d0d8708b54c797f3283062cedb3eac11
zunhogu <zunhogu@gmail.com>
2023-03-07 14:49:23 +0900

    20230307

--- Programmers/lv3/lv3_103 기지국 설치.cpp
+++ Programmers/lv3/lv3_103 기지국 설치.cpp
@@ -13,7 +13,6 @@ int solution(int n, vector<int> stations, int w)
         answer += dist / offset;
         if(dist % offset != 0) answer++;
     }
-    
     for(int i=0; i<stations.size(); i++)
     {
         if(i+1 < stations.size() && stations[i] + w + 1 < stations[i+1] - w)
@@ -23,7 +22,6 @@ int solution(int n, vector<int> stations, int w)
             if(dist % offset != 0) answer++;
         }
     }
-    
     if(stations[stations.size()-1] + w <=n) 
     {
         dist = n - (stations[stations.size()-1] + w);
