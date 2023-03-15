23c81e2fee1f464b39f2d28ad431f2a735acb990
zunhogu <zunhogu@gmail.com>
2023-03-15 12:55:23 +0900

    20230315

---
+++ Programmers/lv3/lv3_106 스티커 모으기(2).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <iostream>          
+using namespace std;
+
+int max = 0;
+
+int solution(vector<int> sticker)
+{
+    int answer =0;
+    
+    int size = sticker.size();
+    vector<int> dp(size, 0);
+    
+    for(int i=0; i<size; i++)
+    {
+        dp[i] += sticker[i] - sticker[(i-1)%size];
+        dp[i] += sticker[i] - sticker[(i+1)%size];
+    }
+    
+    for(int i=0; i<dp.size(); i++)
+        cout << dp[i] << " ";
+    cout << endl;
+    return answer;
+}
