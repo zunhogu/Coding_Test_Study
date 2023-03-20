d63a22d97b705f9930b704b363cdf8ec09f6c59f
zunhogu <zunhogu@gmail.com>
2023-03-20 12:26:24 +0900

    20230320

--- Programmers/lv3/lv3_106 스티커 모으기(2).cpp
+++ Programmers/lv3/lv3_106 스티커 모으기(2).cpp
@@ -1,27 +1,22 @@
-#include <iostream>
 #include <vector>
-#include <iostream>          
 using namespace std;
-
-int max = 0;
 
 int solution(vector<int> sticker)
 {
     int answer =0;
     
-    int size = sticker.size();
-    vector<int> dp(size, 0);
+    int size= sticker.size();
+    if(size==1) return sticker[0];
     
-    for(int i=0; i<size; i++)
-    {
-        dp[i] += sticker[i] - sticker[(i-1)%size];
-        dp[i] += sticker[i] - sticker[(i+1)%size];
-    }
-
-    for(int i=0; i<dp.size(); i++)
-        cout << dp[i] << " ";
-
-    cout << endl;
+    vector<int> dp1(size, 0), dp2(size, 0);
     
-    return answer;
+    dp1[0] = sticker[0];
+    dp1[1] = dp1[0];
+    for(int i=2; i<size-1; i++)
+        dp1[i] = max(dp1[(i-2)%size] + sticker[i], dp1[(i-1)%size]);
+    
+    for(int i=1; i<size; i++)
+        dp2[i] = max(dp2[(i-2)%size] + sticker[i], dp2[(i-1)%size]);
+    
+    return max(dp1[size-2], dp2[size-1]);
 }
