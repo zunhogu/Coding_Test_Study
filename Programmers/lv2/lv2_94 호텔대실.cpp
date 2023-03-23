6e8bb975464abeba053ec19a7a5934d4352b650f
zunhogu <zunhogu@gmail.com>
2023-03-23 15:28:57 +0900

    20230323

--- Programmers/lv2/lv2_94 호텔대실.cpp
+++ Programmers/lv2/lv2_94 호텔대실.cpp
@@ -1,58 +1,45 @@
 #include <string>
 #include <vector>
-#include <iostream>
 using namespace std;
 
-bool book(int start, int end, int book_start, int book_end)
+void book(int start, int end, vector<vector<bool>>& rooms)
 {
-    bool result = true;
-    
-    if(start < book_start && end > book_start && end < book_end)
-        result = false;
-    else if(start > book_start && start < book_end && end > book_end)
-        result = false;
-    else if(start > book_start && end < book_end)
-        result = false;
-    else if(start < book_start && start > book_end)
-        result = false;
-    
-    return result;
-}
-
-int solution(vector<vector<string>> book_time) {
-    int answer = 0;
-    
-    vector<vector<pair<int, int>>> rooms;
-    
-    for(int i=0; i<book_time.size(); i++)
+    for(int i=0; i<rooms.size(); i++)
     {
-        int start = stoi(book_time[i][0].substr(0,2) + book_time[i][0].substr(3,5));
-        int end = stoi(book_time[i][1].substr(0,2) + book_time[i][1].substr(3,5)) + 10;
-        
-        for(int j=0; j<rooms.size(); j++)
+        bool enable = true;
+        for(int j=start+1; j<=end; j++)
         {
-            bool success = false;
-            for(int k=0; k<rooms[j].size(); k++)
+            if(rooms[i][j])
             {
-                cout << start << " ~ " << end << " -> " << rooms[j][k].first << " ~ " << rooms[j][k].second;
-                if(book(start, end, rooms[j][k].first, rooms[j][k].second))
-                {
-                    cout << " 가능 "  << endl;
-                    break;
-                    rooms[j].push_back(make_pair(start, end));
-                    success = true;
-                }
-                cout << " 불가능 " << endl;
+                enable = false;
+                break;
             }
         }
-            
-        if(!success)
+        if(enable)
         {
-            vector<pair<int, int>> new_room;
-            new_room.push_back(make_pair(start, end));
-            rooms.push_back(new_room);
+            for(int j=start; j<=end; j++)
+                rooms[i][j] = true;
+            return;
         }
     }
     
-    return answer;
+    vector<bool> tmp(1499, false);
+    for(int i=start; i<=end; i++)
+        tmp[i] = true;
+    rooms.push_back(tmp);
+    return;
+}
+
+int solution(vector<vector<string>> book_time) {
+    vector<vector<bool>> rooms;
+    
+    for(int i=0; i<book_time.size(); i++)
+    {
+        int start = stoi(book_time[i][0].substr(0,2))*60 + stoi(book_time[i][0].substr(3,5))/60 + stoi(book_time[i][0].substr(3,5))%60;
+        int end = stoi(book_time[i][1].substr(0,2))*60 + (stoi(book_time[i][1].substr(3,5))+10)/60*60 + (stoi(book_time[i][1].substr(3,5)) + 10)%60;
+        
+        book(start, end, rooms);
+    }
+    
+    return rooms.size();
 }
